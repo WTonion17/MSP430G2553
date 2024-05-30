98fcb164d008eda5f3fe813e6551d3a227695202
Dennis <118417252+WTonion17@users.noreply.github.com>
2024-05-31 00:06:55 +0700

    Update main.c

--- MSP430G2553/main.c
+++ MSP430G2553/main.c
@@ -7,10 +7,10 @@
 #define TRIG BIT0                               //P1.0
 #define ECHO BIT1                               //P1.1
 
-#define IN1 BIT3                                //P1.3
+#define IN1 BIT3                                //P1.2
 
-#define MOTOR_FORWARD BIT0                      //P2.0
-#define MOTOR_REVERSE BIT1                      //P2.1
+#define MOTOR_FORWARD BIT1                      //P2.1
+#define MOTOR_REVERSE BIT0                      //P2.0
 
 #define RAIN_SENSOR BIT2                        //P2.2
 
@@ -21,9 +21,6 @@
 unsigned int distance;
 unsigned int Maindistance;
 unsigned int Maxdistance = 205;
-
-enum MotorState { STOPPED, FORWARD, REVERSE };
-volatile enum MotorState motorState = STOPPED;
 
 unsigned char customChar[8] = {
   0x00,
@@ -47,45 +44,45 @@ unsigned char customChar2[8] = {
   0x0E
 };
 
-bool isBlindOpen() {                        // Hàm kiem tra trang thái cua rèm
-    if (P2IN & RAIN_SENSOR) {               // Neu chan cam bien rem dong, tra ve false (rèm dóng)
+bool isBlindOpen() {
+    if (P2IN & RAIN_SENSOR) {
         return true;
     } else {
         return false;
     }
 }
 
-bool digital1() {                           // Hàm kiem tra trang thái cua cambien
-    if (P2IN &  RAIN_SENSOR) {              // Neu chan cam bien co nuoc, tra ve false (mua)
+bool digital1() {
+    if (P2IN &  RAIN_SENSOR) {
         return true;
     } else {
         return false;
     }
 }
 
+//Hàm hiển thị thông báo mưa lên LCD
 void getRain(){
       bool cambienmua = digital1();
           if(cambienmua){
-              LCD_SetCursor(0,2);           //set vi tri lcd(cot, hang)
-              LCD_Print("Sunny");           //in chuoi ra mang hinh
-              LCD_writeChar(8);             //in char ra man hinh
+              LCD_SetCursor(0,2);
+              LCD_Print("Sunny");
+              LCD_writeChar(8);
           }else{
-              LCD_SetCursor(0,2);           //set vi tri lcd(cot, hang)
+              LCD_SetCursor(0,2);
               LCD_Print("Rain ");
-              LCD_writeChar(6);             //in char ra man hinh
+              LCD_writeChar(6);
           }
           __delay_cycles(100000);
 
-          bool isOpen = isBlindOpen();      // dieu khien rem
+          bool isOpen = isBlindOpen();
           if (isOpen){
-              LCD_SetCursor(0,3);           //set vi tri lcd(cot, hang)
-              LCD_Print("Rem mo ra   ");    //in chuoi ra mang hinh
+              LCD_SetCursor(0,3);
+              LCD_Print("Rem mo ra   ");
           }else{
-              LCD_SetCursor(0,3);           //set vi tri lcd(cot, hang)
+              LCD_SetCursor(0,3);
               LCD_Print("Rem dong lai");
           }
 }
-
 
 void delayMicroseconds(unsigned int us) {
     while (us--) {
@@ -93,7 +90,7 @@ void delayMicroseconds(unsigned int us) {
     }
 }
 
-
+//Hàm tính toán mực nước
 unsigned int getDistance() {
     unsigned int duration;
     P1OUT |= TRIG;                          // Set TRIG_PIN high
@@ -104,48 +101,60 @@ unsigned int getDistance() {
     TA0R = 0;                               // Reset timer
     TA0CTL |= MC_2;                         // Start timer in continuous mode
 
-    while (P1IN & ECHO);                    // Wait for ECHO_PIN to go lo
+    while (P1IN & ECHO);                    // Wait for ECHO_PIN to go
     TA0CTL &= ~MC_2;                        // Stop timer
     duration = TA0R;                        // Read timer value
+
     distance = (duration /58);
     Maindistance = Maxdistance - distance;
     return Maindistance;
 }
 
+//Hàm điều khiển motor bơm nước
 void controlMotor(){
     unsigned int distance = getDistance();
 
-    LCD_SetCursor(0,0);                      //set vi tri lcd(cot, hang)
-    LCD_Print("Distance: ");                 //in chuoi ra mang hinh
-    lcd_put_num(Maindistance,0,0);
-    LCD_Print("cm ");
+    LCD_SetCursor(0,0);
+    LCD_Print("Distance: ");
+    lcd_put_num(distance,0,0);
+    LCD_Print("cm     ");
 
-      // Điều khiển động cơ bơm nước
     if (distance < 200) {
-        P1OUT |= IN1;                        // Turn on motor
-        LCD_SetCursor(0,1);                  //set vi tri lcd(cot, hang)
-        LCD_Print("Bom Nuoc Vao       ");    //in chuoi ra mang hinh
+        P1OUT |= IN1;
+        LCD_SetCursor(0,1);
+        LCD_Print("Bom Nuoc Vao       ");
         } else {
-            P1OUT &= ~IN1;                   // Turn off motor
-            LCD_SetCursor(0,1);              //set vi tri lcd(cot, hang)
-            LCD_Print("Khong Bom,Khong Hut");//in chuoi ra mang hinh
+            P1OUT &= ~IN1;
+            LCD_SetCursor(0,1);
+            LCD_Print("Khong Bom, Khong Hut");
             }
 }
 
+//Hàm điều khiển mở, đóng rèm
+void openCurtain() {
+    P2OUT |= MOTOR_FORWARD;
+    P2OUT &= ~MOTOR_REVERSE;
+}
+void closeCurtain() {
+    P2OUT |= MOTOR_REVERSE;
+    P2OUT &= ~MOTOR_FORWARD;
+}
 
-void setup() {
+//Hàm setup chân
+void setup_dc() {
     P1SEL = ECHO + TRIG;
     P1SEL2 = ECHO ;
-    P1DIR |= TRIG;                            // Set TRIG_PIN là output
-    P1OUT &= ~TRIG;                           // Set TRIG_PIN thành mức thấp
+    P1DIR |= TRIG;
+    P1OUT &= ~TRIG;
 
-    P1DIR &= ~ECHO;                           // Set ECHO_PIN là input
-    P1SEL &= ~ECHO;                           // Set chân GPIO cho chân ECHO_PIN
+    P1DIR &= ~ECHO;
+    P1SEL &= ~ECHO;
 
-    P1DIR |= IN1 ;                            //cấu hình các chân động cơ là OUTPUT
-    P1OUT &= ~IN1;                            // Khởi tạo các chân điều khiển động cơ ở mức thấp
+    P1DIR |= IN1 ;
+    P1OUT &= ~IN1;
 
     P2IFG = 0x00;
+
     P2DIR |= MOTOR_FORWARD + MOTOR_REVERSE;
     P2OUT &= ~(MOTOR_FORWARD + MOTOR_REVERSE);
 
@@ -168,14 +177,14 @@ void main(void){
     WDTCTL = WDTPW + WDTHOLD;
     BCSCTL1 = CALBC1_1MHZ;
     DCOCTL = CALDCO_1MHZ;
-    TA0CCR0 = 0;                              // Initialize CCR0
-    TA0CTL = TASSEL_2 + MC_0;                 // SMCLK, Stop mode
+    TA0CCR0 = 0;
+    TA0CTL = TASSEL_2 + MC_0;
 
-    setup();
+    setup_dc();
 
-    LCD_Init(0X27, 4, 20);                    //khoi tao LCD voi giao thuc i2c
-    LCD_backlightOn();                        //cho phep bat den nen
-    LCD_Clear();                              //clear mang hinh de xoa ky tu vo dinh
+    LCD_Init(0X27, 4, 20);
+    LCD_backlightOn();
+    LCD_Clear();
 
     LCD_createChar(8, customChar);
     LCD_createChar(6, customChar2);
@@ -187,33 +196,26 @@ void main(void){
   }
 }
 
-
 #pragma vector = PORT2_VECTOR
 __interrupt void Port_2(void){
     if (P2IFG & RAIN_SENSOR) {
-        if (motorState == FORWARD) {
-            motorState = REVERSE;
-            P2OUT &= ~MOTOR_FORWARD;
-            P2OUT |= MOTOR_REVERSE;
-        } else if (motorState == REVERSE) {
-            motorState = FORWARD;
-            P2OUT &= ~MOTOR_REVERSE;
-            P2OUT |= MOTOR_FORWARD;
-        } else {
-            motorState = FORWARD;
-            P2OUT |= MOTOR_FORWARD;
-        }
         P2IFG &= ~RAIN_SENSOR;
+        if (P2IN & RAIN_SENSOR) {
+            openCurtain();
+        } else {
+            closeCurtain();
+        }
     }
-
     if(P2IFG & LIMIT_SWITCH_PIN1) {
-        P2OUT &= ~(MOTOR_FORWARD + MOTOR_REVERSE);
-        motorState = STOPPED;
         P2IFG &= ~LIMIT_SWITCH_PIN1;
+        if (P2IN & LIMIT_SWITCH_PIN1) {
+            P2OUT &= ~(MOTOR_FORWARD + MOTOR_REVERSE);
+        }
     }
     if (P2IFG & LIMIT_SWITCH_PIN2) {
-        P2OUT &= ~(MOTOR_FORWARD + MOTOR_REVERSE);
-        motorState = STOPPED;
         P2IFG &= ~LIMIT_SWITCH_PIN2;
+        if (P2IN & LIMIT_SWITCH_PIN2) {
+            P2OUT &= ~(MOTOR_FORWARD + MOTOR_REVERSE);
+        }
     }
 }
